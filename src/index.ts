5c0b49baa0d758d6574dd92bc49961fece1472e5
davids199005 <davids199005@gmail.com>
2026-04-18 18:02:37 +0300

    feat: add Prettier for code formatting and update CLI and index files for consistency

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-export const VERSION = "0.0.1"
+export const VERSION = '0.0.1';
 
 export function greet(name: string): string {
-    return `Hello From Xpressify, ${name}!`;
+  return `Hello From Xpressify, ${name}!`;
 }
